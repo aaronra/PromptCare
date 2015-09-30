e705222428db1367b11218184c42ee537db6f263
aaronra <aaronrandrup@gmail.com>
2015-09-30 10:24:10 +0800

    fixed collapsible

--- app/client/templates/layouts/master_layout/master_layout.js
+++ app/client/templates/layouts/master_layout/master_layout.js
@@ -8,6 +8,6 @@ Template.MasterLayout.events({
 Template.MasterLayout.onRendered(function () {
     $('.button-collapse').sideNav();
     $('.collapsible').collapsible({
-        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        accordion: false
     });
 });
